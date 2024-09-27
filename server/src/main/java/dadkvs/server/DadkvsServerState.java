82930359724b48e6cab0e3f64054b140251813ee
Dageus <101069446+Dageus@users.noreply.github.com>
2024-09-27 23:21:27 +0100

    some more TODO's

--- server/src/main/java/dadkvs/server/DadkvsServerState.java
+++ server/src/main/java/dadkvs/server/DadkvsServerState.java
@@ -2,7 +2,7 @@ package dadkvs.server;
 
 public class DadkvsServerState {
 	boolean i_am_leader;
-	final int n_servers;
+	int n_servers;
 	int debug_mode;
 	int base_port;
 	int my_id;
@@ -11,8 +11,10 @@ public class DadkvsServerState {
 	MainLoop main_loop;
 	Thread main_loop_worker;
 
-	private Sequencer _sequencer = new Sequencer();
-	private Queue _queue = new Queue();
+	private Sequencer _sequencer;
+	private Queue _queue;
+	// TODO: possibly add hash table as a backlog to map ids that have not yet been
+	// executed
 
 	public DadkvsServerState(int kv_size, int port, int myself) {
 		base_port = port;
@@ -25,6 +27,9 @@ public class DadkvsServerState {
 		main_loop = new MainLoop(this);
 		main_loop_worker = new Thread(main_loop);
 		main_loop_worker.start();
+
+		_sequencer = new Sequencer();
+		_queue = new Queue();
 	}
 
 	public int getSequencerNumber() {
@@ -40,6 +45,7 @@ public class DadkvsServerState {
 	}
 
 	public void orderRequest(int reqId, int epoch) {
-		// TODO: don't know how to do this :)))))
+		// TODO: if when epoch == nextEpoch, execute, else, wait() until right epoch
+		// nextEpoch should be a variable in server state
 	}
 }

cb13cd3687cdadd010063dc9d97c72050378ea6d
Dageus <101069446+Dageus@users.noreply.github.com>
2024-09-30 12:12:11 +0100

    new business

--- server/src/main/java/dadkvs/server/DadkvsMainServiceImpl.java
+++ server/src/main/java/dadkvs/server/DadkvsMainServiceImpl.java
@@ -47,36 +47,39 @@ public class DadkvsMainServiceImpl extends DadkvsMainServiceGrpc.DadkvsMainServi
 		int writekey = request.getWritekey();
 		int writeval = request.getWriteval();
 
-		server_state.waitInLine(server_state.getSequencerNumber());
-
 		// for debug purposes
 		System.out.println("reqid " + reqid + " key1 " + key1 + " v1 " + version1 + " k2 " + key2 + " v2 " + version2
 				+ " wk " + writekey + " writeval " + writeval);
 
-		this.timestamp++;
-		TransactionRecord txrecord = new TransactionRecord(key1, version1, key2, version2, writekey, writeval,
-				this.timestamp);
-
 		if (server_state.i_am_leader) {
 			// if leader, send fast paxos request with sequence number as epoch
+			int seqNumber = server_state.getSequencerNumber();
+			server_state.waitInLine(seqNumber);
+
+			this.timestamp++;
+			TransactionRecord txrecord = new TransactionRecord(key1, version1, key2, version2, writekey, writeval,
+					this.timestamp);
+
+			boolean result = this.server_state.store.commit(txrecord);
+
+			server_state.nextInLine();
 
 			// for debug purposes
 			System.out.println("Result is ready to be ordered by leader with reqid " + reqid);
 
-			server_state.orderIdRequest(reqid, server_state.getSequencerNumber());
-			server_state.nextInLine();
+			server_state.orderIdRequest(reqid, seqNumber);
 
-			boolean result = this.server_state.store.commit(txrecord);
+			DadkvsMain.CommitReply response = DadkvsMain.CommitReply.newBuilder()
+					.setReqid(reqid).setAck(result).build();
+
 		} else {
 			// if slave server, add to queue? and wait until you receive a request from the
 			// client that the leader has ordered
+			server_state.waitInLine(seqNumber);
 			if (/* received request matching order set by leader */ false) {
 				boolean result = this.server_state.store.commit(txrecord);
 			}
 		}
-
-		DadkvsMain.CommitReply response = DadkvsMain.CommitReply.newBuilder()
-				.setReqid(reqid).setAck(result).build();
 
 		responseObserver.onNext(response);
 		responseObserver.onCompleted();

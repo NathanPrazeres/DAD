0fff733c8118c25709617f275c3b51af4ad7ffc1
Nathaniel Prazeres <nathanprazeres@gmail.com>
2024-10-01 10:18:07 +0100

    Removed duplicate code

--- server/src/main/java/dadkvs/server/DadkvsMainServiceImpl.java
+++ server/src/main/java/dadkvs/server/DadkvsMainServiceImpl.java
@@ -29,10 +29,6 @@ public class DadkvsMainServiceImpl extends DadkvsMainServiceGrpc.DadkvsMainServi
 	public void read(DadkvsMain.ReadRequest request, StreamObserver<DadkvsMain.ReadReply> responseObserver) {
 		// for debug purposes
 		System.out.println("Receiving read request:" + request);
-	@Override
-	public void read(DadkvsMain.ReadRequest request, StreamObserver<DadkvsMain.ReadReply> responseObserver) {
-		// for debug purposes
-		System.out.println("Receiving read request:" + request);
 
 		int reqid = request.getReqid();
 		int key = request.getKey();
@@ -55,10 +51,6 @@ public class DadkvsMainServiceImpl extends DadkvsMainServiceGrpc.DadkvsMainServi
 		responseObserver.onCompleted();
 	}
 
-	@Override
-	public void committx(DadkvsMain.CommitRequest request, StreamObserver<DadkvsMain.CommitReply> responseObserver) {
-		// for debug purposes
-		System.out.println("Receiving commit request:" + request);
 	@Override
 	public void committx(DadkvsMain.CommitRequest request, StreamObserver<DadkvsMain.CommitReply> responseObserver) {
 		// for debug purposes
@@ -92,10 +84,6 @@ public class DadkvsMainServiceImpl extends DadkvsMainServiceGrpc.DadkvsMainServi
 			// if leader, send fast paxos request with sequence number as epoch
 			int seqNumber = server_state.getSequencerNumber();
 			server_state.waitInLine(seqNumber);
-		if (server_state.i_am_leader) {
-			// if leader, send fast paxos request with sequence number as epoch
-			int seqNumber = server_state.getSequencerNumber();
-			server_state.waitInLine(seqNumber);
 
 			this.timestamp++;
 			TransactionRecord txrecord = new TransactionRecord(key1, version1, key2, version2, writekey, writeval,
@@ -104,34 +92,7 @@ public class DadkvsMainServiceImpl extends DadkvsMainServiceGrpc.DadkvsMainServi
 			boolean result = this.server_state.store.commit(txrecord);
 
 			server_state.nextInLine();
-			this.timestamp++;
-			TransactionRecord txrecord = new TransactionRecord(key1, version1, key2, version2, writekey, writeval,
-					this.timestamp);
 
-			boolean result = this.server_state.store.commit(txrecord);
-
-			server_state.nextInLine();
-
-			// for debug purposes
-			System.out.println("Result is ready to be ordered by leader with reqid " + reqid);
-
-			server_state.orderIdRequest(reqid, seqNumber);
-
-			DadkvsMain.CommitReply response = DadkvsMain.CommitReply.newBuilder()
-					.setReqid(reqid).setAck(result).build();
-
-		} else {
-			// if slave server, add to queue? and wait until you receive a request from the
-			// client that the leader has ordered
-			server_state.waitInLine(seqNumber);
-			if (/* received request matching order set by leader */ false) {
-				boolean result = this.server_state.store.commit(txrecord);
-			}
-		}
-
-		responseObserver.onNext(response);
-		responseObserver.onCompleted();
-	}
 			// for debug purposes
 			System.out.println("Result is ready to be ordered by leader with reqid " + reqid);
 
